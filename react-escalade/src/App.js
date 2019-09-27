97c60bb5bbb08899264ec76f21c6ddfc1470a569
krakenfrancois <froxworld@gmail.com>
2019-09-27 13:07:34 +0200

    modify menu bar on react

--- react-escalade/src/App.js
+++ react-escalade/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
-
+import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
 import logo from './logo.svg'
 import './App.css'
@@ -12,22 +12,36 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <table>
+            <tbody>
             <tr>
               <td><h1 className="App-title">Climbing Zone</h1></td>
               <td><img src={logo} className="App-logo" alt="climbing Zone logo"/></td>
               <td>
                 <div>
                   <ButtonToolbar>
-                    <Button variant="primary" size="lg">
-                      Large button
+                    <Button variant="dark" size="lg">
+                      <Image className="menu-button "    src="login.jpg" rounded></Image>
                     </Button>
-                    <Button variant="secondary" size="lg">
-                      Large button
+                    <Button variant="dark" size="lg">
+                      <Image className="menu-button "    src="grimpeur.jpg" rounded></Image>
                     </Button>
+                    <Button variant="dark" size="lg">
+                      <Image className="menu-button "    src="montagne.png" rounded></Image>
+                    </Button>
+                    <Button variant="dark" size="lg">
+                      <Image className="menu-button "    src="parametre.png" rounded></Image>
+                    </Button>
+                    <Button variant="dark" size="lg">
+                      <Image className="menu-button "    src="reussite.png" rounded></Image>
+                    </Button>
+                    <Button variant="dark" size="lg">
+                    <Image className="menu-button "    src="time.png" rounded></Image>
+                  </Button>
                   </ButtonToolbar>
                 </div>
               </td>
             </tr>
+            </tbody>
           </table>
         </header>
       </div>
